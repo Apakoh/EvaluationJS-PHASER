f1c31887813dcb9f9315ffb65bcd852c39d24f43
Apakoh_Quentin-Gerome <quentin.bartschs@gmail.com>
2019-10-17 10:47:25 +0200

    Rework Timer UI creation

--- JS/First_Scene.js
+++ JS/First_Scene.js
@@ -15,13 +15,10 @@ class First_Scene extends Phaser.Scene
   create()
   {
     currentGame = this;
-    background = this.add.tileSprite(0, 0, windowWidth*2, windowHeight*2, 'background');
-    initTime = timeCounter();
+    background = currentGame.add.tileSprite(0, 0, windowWidth*2, windowHeight*2, 'background');
+    gestionUIRealTime();
+    startTimer(0);
   }
-
-
-  /* ------------------------------------------ */
-
 
 
   /* ------------------------------------------ */
@@ -35,21 +32,14 @@ class First_Scene extends Phaser.Scene
       // Moving Background
       background._tilePosition.x += 2;
 
-      // Timer update
-      currentTimer = currentTimerTemps + timeCounter() - initTime;
+      // UI Gestion
+      gestionUIRealTime();
 
       // Target Creation
-      console.log(currentTimer);
       if(nbTargetsTemp == 0)
       {
-        createTarget(this, 1 + currentTimer/difficulty);
+        createTarget(1 + timer/difficulty);
       }
-    }
-    else
-    {
-      // Gestion of pause consequences on Timer
-      currentTimerTemps = currentTimer;
-      initTime = timeCounter();
     }
   }
 
@@ -68,6 +58,10 @@ var pause = false;
 var nbTargetsTemp = 0;
 const minimalTimeSpawn = 1000;
 
+// Text UI
+var timerUI;
+var scoreUI;
+
 // Difficulty
 const difficulty = 10;
 
@@ -75,10 +69,8 @@ const difficulty = 10;
 var keySpace;
 
 // Time
-var initTime;
-var pauseTime = 0;
-var currentTimer = 0;
-var currentTimerTemps = 0;
+var timer = 0;
+var clock;
 
 function timeCounter()
 {
@@ -107,20 +99,48 @@ function randomInteger(range)
   return Math.floor(Math.random() * Math.floor(range+1));
 }
 
-function createTarget(thisGame, nbSpawnTargets)
+function createTarget(nbSpawnTargets)
 {
   for(var i=0; i < nbSpawnTargets; i++)
   {
     // Target Creation
-    target = thisGame.add.sprite(randomInteger(windowWidth), randomInteger(windowHeight), 'target').setScale(0.2);
+    target = currentGame.add.sprite(randomInteger(windowWidth), randomInteger(windowHeight), 'target').setScale(0.2);
     target.setInteractive();
     target.on('pointerdown', destroyTarget);
     // Ajout Timer
-    thisGame.time.addEvent({delay: randomInteger(2000) + minimalTimeSpawn,callback: deleteTarget,args: [target]});
+    currentGame.time.addEvent({delay: randomInteger(2000) + minimalTimeSpawn, callback: deleteTarget, args: [target]});
     // Count
     nbTargets += 1;
     nbTargetsTemp +=1;
   }
+}
+
+function gestionUIRealTime()
+{
+  // Timer UI
+  if(timerUI != undefined)
+  {
+    timerUI.destroy();
+  }
+  timerUI = currentGame.add.text(windowWidth*0.03, windowHeight*0.03, 'Timer : ' + timer, { fontFamily: '"Roboto Condensed"', fontSize: 36 });
+
+  // Score UI
+  if(scoreUI != undefined)
+  {
+    scoreUI.destroy();
+  }
+  scoreUI = currentGame.add.text(windowWidth*0.03, windowHeight*0.07, 'Score : ' + hit + ' / ' + nbTargets, { fontFamily: '"Roboto Condensed"', fontSize: 34 });
+}
+
+function startTimer(timeCount)
+{
+  timer += timeCount;
+  console.log(timer);
+  clock = currentGame.time.addEvent({
+    delay: 1000,
+    callback: startTimer,
+    args: [1]
+  });
 }
 
 function pauseGame()
